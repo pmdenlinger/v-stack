9dd486649d14fb761a18a1aaa4c72ba1c9d51cfe
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-22 11:20:34 -0700

    Initial Commit

---
+++ v-stack/v_stackApp.swift
@@ -0,0 +1,17 @@
+//
+//  v_stackApp.swift
+//  v-stack
+//
+//  Created by user on 6/22/21.
+//
+
+import SwiftUI
+
+@main
+struct v_stackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
